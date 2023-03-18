bad2c5eebc482064933126074bfd60c665e57f43
Valeria Dubina <valeria.dubina@isa.utm.md>
2023-03-18 11:53:21 +0200

    Add workspace edit

--- app.js
+++ app.js
@@ -20,34 +20,35 @@ class Workplace {
         this.title = title ?? "Untitled" + this.id;
     }
 
-    get() {
-        return {
-            "id": this.id,
-            "title": this.title
-        }
-    }
-
     getHtmlTemplate() {
         return `
-        <div class="workspace">
-            ${this.title}
-            <i class="workspace__delete-action bi bi-x"></i>
-        </div> 
+            <div class="workspace">
+               <input class="workplace__input" value="${this.title}">
+                <i class="workspace__delete-action bi bi-x"></i>
+            </div> 
         `
     }
 }
 
 class WorkspacesStorage {
+    activeWorkplace = {}
     workplaces = [];
 
     workspacesList = document.querySelector("#workspaces-list");
 
     loadWorkplaces() {
         this.workplaces = JSON.parse(localStorage.getItem("tasks"));
+        this.activeWorkplace = JSON.parse(localStorage.getItem("active_workspace"))
 
         if (!this.workplaces?.length) {
             this.workplaces = [new Workplace()];
             localStorage.setItem("tasks", JSON.stringify(this.workplaces))
+        }
+
+
+        if (!this.activeWorkplace) {
+            this.activeWorkplace = this.workplaces[0];
+            localStorage.setItem("active_workspace", JSON.stringify(this.activeWorkplace))
         }
 
         this.displayWorkplaces()
@@ -59,7 +60,26 @@ class WorkspacesStorage {
         this.workplaces.forEach(workplace => {
                 const element = document.createElement('div')
                 element.innerHTML = new Workplace(workplace.id, workplace.title).getHtmlTemplate();
-                element.addEventListener('click', () => this.removeWorkspace(workplace.id))
+
+                if (this.activeWorkplace?.id === workplace.id) {
+                    element.querySelector(".workspace").classList.add('workspace--active');
+                }
+
+
+                element.querySelector(".workplace__input").addEventListener(
+                    'input',
+                    e => this.updateWorkspace(workplace, e.target.value)
+                )
+                element.addEventListener(
+                    'click',
+                    () => this.setActiveWorkspace(workplace, element)
+                )
+                element.querySelector('.workspace__delete-action').addEventListener(
+                    "click",
+                    () => this.removeWorkspace(workplace.id)
+                )
+
+
                 this.workspacesList.appendChild(element);
             }
         )
@@ -76,7 +96,37 @@ class WorkspacesStorage {
         this.workplaces = this.workplaces.filter(workplace => workplace.id !== id);
         localStorage.setItem("tasks", JSON.stringify(this.workplaces));
 
+        if (this.activeWorkplace.id === id) {
+            this.activeWorkplace = {};
+            localStorage.setItem("active_workspace", JSON.stringify(this.activeWorkplace))
+        }
+
         this.displayWorkplaces();
+    }
+
+    updateWorkspace(workplace, title) {
+        let element = this.workplaces.find(element => element.id === workplace.id)
+        element.title = title;
+
+        localStorage.setItem("tasks", JSON.stringify(this.workplaces));
+
+        if (this.activeWorkplace.id === workplace.id) {
+            this.activeWorkplace.title = title;
+            localStorage.setItem("active_workspace", JSON.stringify(this.activeWorkplace))
+        }
+    }
+
+    setActiveWorkspace(workplace, element) {
+        this.activeWorkplace = workplace;
+
+        localStorage.setItem("active_workspace", JSON.stringify(workplace))
+        let activeElements = document.getElementsByClassName("workspace--active");
+
+        if (activeElements.length > 0) {
+            activeElements[0].classList.remove("workspace--active");
+        }
+
+        element.querySelector(".workspace").classList.add('workspace--active');
     }
 }
 
